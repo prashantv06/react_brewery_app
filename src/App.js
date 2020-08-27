7c8a1e452851fa2f8fcc6124235c9029c3963ed7
Prashant <prasant.irl@gmail.com>
2020-08-27 15:21:29 +0100

    header changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import { Jumbotron, Button } from 'reactstrap';
+import React, { Component } from 'react';
+import { Jumbotron } from 'reactstrap';
 import './App.css';
+//import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <Jumbotron>
-        <h1>The Random Beer App</h1>
-        <p>
-          If true love had a taste, it would be a cold beer.
-        </p>
-        <p>
-          <Button className="random_button">Learn more</Button>
-        </p>
-      </Jumbotron>
-    </div>
-  );
+class App extends Component {
+
+  getStyle = () => {
+    return {
+      fontSize: '3vw',
+      backgroundColor: '#000',
+      color: '#fff'
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Jumbotron>
+          <h1>The Random Beer App</h1>
+          <p>
+            If true love had a taste, it would be a cold beer.
+          </p>
+          <p>
+          <button type="button" class="btn btn-secondary" style={ this.getStyle() }>
+          Get Random Beer
+          </button>
+          </p>
+        </Jumbotron>
+      </div>
+    );
+  }  
 }
 
 export default App;
